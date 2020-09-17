7540da43503267695f927dc6a175c51d39cd6423
ellreka <ellreka5364@gmail.com>
2020-09-18 01:11:24 +0900

    feat: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,20 @@
 module.exports = {
-  extends: 'standard-with-typescript',
+  extends: [
+    'standard',
+    'standard-with-typescript',
+    'standard-react',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'prettier/standard'
+  ],
+  plugins: [
+    'prettier',
+    '@typescript-eslint',
+    'simple-import-sort',
+    'react-hooks',
+    'jsx-a11y'
+  ],
   parserOptions: {
     project: './tsconfig.json'
   }
