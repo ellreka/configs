ea812d3b1c0b594b68a1da5f734ee132481e8d9d
ellreka <ellreka5364@gmail.com>
2021-03-12 02:26:08 +0900

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,26 @@
+const colors = require('tailwindcss/colors')
+const theme = require('tailwindcss')
+
 module.exports = {
-  theme: {},
-  variants: {},
+  theme: {
+    extend: {
+      colors: {
+        blueGray: colors.blueGray,
+        coolGray: colors.coolGray,
+        orange: colors.orange
+      },
+      minWidth: {
+        ...theme.spacing
+      }
+    }
+  },
+  variants: {
+    extend: {
+      borderWidth: ['hover', 'focus'],
+      opacity: ['disabled'],
+      pointerEvents: ['disabled'],
+      cursor: ['disabled']
+    }
+  },
   plugins: []
 }
